e880358fecbef36dee2eeab947349a977f6287a1
Evgenii Kolesnichenko <129544881+Laqwalasse@users.noreply.github.com>
2024-09-01 07:46:19 +0500

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -1,4 +1,4 @@
-function socket1(id) {
+function getSocketValue(id, socketNumber) {
   var url = 'https://www.wowhead.com/wotlk/item=' + id + '&xml';
   var response = UrlFetchApp.fetch(url);
   var xmlContent = response.getContentText();
@@ -10,64 +10,13 @@ function socket1(id) {
     var jsonEquipContent = jsonEquipElement.getText().trim();
     var jsonData = JSON.parse('{' + jsonEquipContent + '}');
 
-    if (jsonData.hasOwnProperty('socket1')) {
-      var socketValue = jsonData['socket1'];
-      Logger.log("The value of socket1 is: " + socketValue);
+    var socketKey = 'socket' + socketNumber; // Формируем ключ для поиска нужного сокета
+    if (jsonData.hasOwnProperty(socketKey)) {
+      var socketValue = jsonData[socketKey];
+      Logger.log("The value of " + socketKey + " is: " + socketValue);
       return socketValue;
     } else {
-      Logger.log("socket1 not found");
-      return '';
-    }
-  } else {
-    Logger.log("No jsonEquip content found");
-    return '';
-  }
-}
-
-function socket2(id) {
-  var url = 'https://www.wowhead.com/wotlk/item=' + id + '&xml';
-  var response = UrlFetchApp.fetch(url);
-  var xmlContent = response.getContentText();
-  var document = XmlService.parse(xmlContent);
-  var root = document.getRootElement();
-  var jsonEquipElement = root.getChild('item').getChild('jsonEquip');
-
-  if (jsonEquipElement) {
-    var jsonEquipContent = jsonEquipElement.getText().trim();
-    var jsonData = JSON.parse('{' + jsonEquipContent + '}');
-
-    if (jsonData.hasOwnProperty('socket2')) {
-      var socketValue = jsonData['socket2'];
-      Logger.log("The value of socket2 is: " + socketValue);
-      return socketValue;
-    } else {
-      Logger.log("socket2 not found");
-      return '';
-    }
-  } else {
-    Logger.log("No jsonEquip content found");
-    return '';
-  }
-}
-
-function socket3(id) {
-  var url = 'https://www.wowhead.com/wotlk/item=' + id + '&xml';
-  var response = UrlFetchApp.fetch(url);
-  var xmlContent = response.getContentText();
-  var document = XmlService.parse(xmlContent);
-  var root = document.getRootElement();
-  var jsonEquipElement = root.getChild('item').getChild('jsonEquip');
-
-  if (jsonEquipElement) {
-    var jsonEquipContent = jsonEquipElement.getText().trim();
-    var jsonData = JSON.parse('{' + jsonEquipContent + '}');
-
-    if (jsonData.hasOwnProperty('socket3')) {
-      var socketValue = jsonData['socket3'];
-      Logger.log("The value of socket3 is: " + socketValue);
-      return socketValue;
-    } else {
-      Logger.log("socket3 not found");
+      Logger.log(socketKey + " not found");
       return '';
     }
   } else {
